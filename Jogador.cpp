ae07a7a8370986ef96ca32c0b0a182d078b17831
João Guilherme <jg0747867@gmail.com>
2026-05-22 03:34:03 -0300

    Implementado parte da classe jogador

--- Jogador.cpp
+++ Jogador.cpp
@@ -1,1 +1,46 @@
 #include "Jogador.h"
+#include "Inimigo.h"
+
+namespace TrabalhoJogo {
+	namespace Entidades {
+		namespace Personagens {
+
+			Jogador::Jogador():
+				pontos(0)
+			{
+				
+			}
+			Jogador::~Jogador()
+			{
+
+			}
+			void Jogador::executar()
+			{
+				mover();
+			}
+			void Jogador::mover()
+			{
+				
+			}
+			void Jogador::salvar()
+			{
+
+			}
+			void Jogador::colidir(Inimigo* inimigo)
+			{
+				if (inimigo)
+				{
+					
+				}
+			}
+			void Jogador::addPontos(int valor)
+			{
+				pontos += valor;
+			}
+			int Jogador::getPontos() const
+			{
+				return pontos;
+			}
+		}
+	}
+}
--- Jogador.h
+++ Jogador.h
@@ -1,5 +1,40 @@
 #pragma once
-class Jogador
-{
-};
+
+#ifndef JOGADOR_H_
+#define JOGADOR_H_
+
+#include "Personagem.h"
+
+namespace TrabalhoJogo {
+	namespace Entidades {
+		namespace Personagens {
+
+				class Inimigo;
+
+				class Jogador : public Personagem
+				{
+				private:
+					int pontos;
+
+				public:
+
+					Jogador();
+					virtual ~Jogador();
+
+					void colidir(Inimigo* inimigo);
+					virtual void executar();
+					virtual void salvar();
+					virtual void mover();
+
+					void addPontos(int valor);
+					int getPontos() const;
+
+				};
+		}
+	}
+}
+#endif
+
+
+
 
