b6f3183480f8e5b38ae8f3d6e3739d426d72c5cf
João Guilherme <jg0747867@gmail.com>
2026-05-23 14:31:43 -0300

    Fiz parte da função inimigo facil

--- InimFacil.cpp
+++ InimFacil.cpp
@@ -1,1 +1,26 @@
 #include "InimFacil.h"
+#include "Jogador.h"
+
+namespace TrabalhoJogo {
+	namespace Entidades {
+		namespace Personagens {
+
+			InimFacil::InimFacil()
+			{
+
+			}
+			InimFacil::~InimFacil()
+			{
+
+			}
+			void InimFacil::executar()
+			{
+
+			}
+			void InimFacil::danificar(Jogador* p)
+			{
+
+			}
+		}
+	}
+}
