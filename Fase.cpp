c3aff44743160f7f2768ce2ad7cbf79219daf36f
João Guilherme <jg0747867@gmail.com>
2026-05-25 18:46:01 -0300

    feito a classe fase, mas parou de mostrar na tela o jogador

--- Fase.cpp
+++ Fase.cpp
@@ -1,1 +1,28 @@
 #include "Fase.h"
+
+namespace TrabalhoJogo {
+	namespace Fases {
+
+		Fase::Fase() {}
+		Fase::~Fase() {}
+
+		void Fase::executar()
+		{
+			
+		}
+		void Fase::salvar()
+		{
+			//salva como a fase esta
+		}
+		void Fase::criaPlataformas()
+		{
+
+		}
+		void Fase::criaCenario()
+		{
+			criaPlataformas();
+			criaCenario();
+		}
+
+	}
+}
--- Fase.h
+++ Fase.h
@@ -1,5 +1,34 @@
 #pragma once
-class Fase
-{
-};
+
+#ifndef FASE_H_
+#define FASE_H_
+
+#include "Ente.h"
+#include "ListaEntidades.h"
+#include "GerenciadorColisoes.h"
+
+
+namespace TrabalhoJogo {
+	namespace Fases {
+
+		class Fase : public Ente
+		{
+		protected:
+			ListaEntidades lista_ents;
+			GerenciadorColisoes Gc;
+			virtual void criaInimigos() = 0;
+			virtual void criaObstaculos() = 0;
+
+			virtual void criaPlataformas();
+			virtual void criaCenario();
+
+		public:
+			Fase();
+			virtual ~Fase();
+			virtual void executar();
+			virtual void salvar();
+		};
+	}
+}
+#endif
 
