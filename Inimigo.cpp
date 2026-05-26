8fe1c9e56faca272a94ccb2962bdb89cc73b0624
GiovanniBueno <gvdutrabueno@gmail.com>
2026-05-26 15:03:16 -0300

    Teste

--- Inimigo.cpp
+++ Inimigo.cpp
@@ -1,1 +1,15 @@
 #include "Inimigo.h"
+
+namespace TrabalhoJogo {
+	namespace Entidades {
+		namespace Personagens {
+			Inimigo::Inimigo() :
+				Personagem(),
+				nivelMaldade(1) {}
+
+			Inimigo::~Inimigo() {}
+
+			void Inimigo::salvarDataBuffer() {} //Implementar Depois 
+		}
+	}
+}
--- Inimigo.h
+++ Inimigo.h
@@ -1,5 +1,31 @@
 #pragma once
-class Inimigo
-{
-};
+
+#ifndef INIMIGO_H_
+#define INIMIGO_H_
+
+#include "Personagem.h"
+
+namespace TrabalhoJogo {
+	namespace Entidades {
+		namespace Personagens {
+			class Jogador;
+
+			class Inimigo : public Personagem {
+			protected:
+				int nivelMaldade;
+			public:
+				Inimigo();
+				~Inimigo();
+
+				void salvarDataBuffer();
+
+				virtual void executar() = 0;
+				virtual void danificar(Jogador* p) = 0;
+				virtual void salvar() = 0;
+			};
+		}
+	}
+}
+
+#endif
 
