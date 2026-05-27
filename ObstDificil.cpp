907ab25a2bc15b3947ac8bb87c2dc1ca6596d57f
João Guilherme <jg0747867@gmail.com>
2026-05-27 01:44:18 -0300

    Criei a class obsDificil, mas n consegui fazer ele aparecer na tela ainda

--- ObstDificil.cpp
+++ ObstDificil.cpp
@@ -1,1 +1,48 @@
 #include "ObstDificil.h"
+#include "Jogador.h"
+
+namespace TrabalhoJogo {
+	namespace Entidades {
+		namespace Obstaculos {
+
+			ObstDificil::ObstDificil() :
+				Obstaculo(),
+				danosidade(2)
+			{
+				danoso = true;
+
+				body.setSize(sf::Vector2f(200.f, 60.f));
+				body.setFillColor(sf::Color::Blue);
+				body.setOutlineColor(sf::Color::White);
+				body.setOutlineThickness(4.f);
+
+				setPosicao(100, 100);
+			}
+
+			ObstDificil::~ObstDificil() {}
+
+			void ObstDificil::executar() {
+				desenhar();
+			}
+
+			void ObstDificil::obstaculizar(Personagens::Jogador* pJogador) {
+				if (!pJogador)
+					return;
+
+				sf::FloatRect corpoJogador = pJogador->getBody().getGlobalBounds();
+				sf::FloatRect corpoObst = body.getGlobalBounds();
+
+				if (corpoJogador.intersects(corpoObst)) {
+					pJogador->receberDano();
+				}
+			}
+			void ObstDificil::salvar()
+			{
+
+			}
+			void ObstDificil::salvarDataBuffer() {
+				Entidade::salvarDataBuffer();
+			}
+		}
+	}
+}
