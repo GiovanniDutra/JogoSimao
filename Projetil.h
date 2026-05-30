7ef0c58186dc207bafdcc27c746d8da6ea987171
João Guilherme <jg0747867@gmail.com>
2026-05-30 04:09:31 -0300

    Implementei a class projetil, ai vou ver se faço a jogo depois

--- Projetil.h
+++ Projetil.h
@@ -1,5 +1,34 @@
 #pragma once
-class Projetil
-{
-};
 
+#ifndef PROJETIL_H_
+#define PROJETIL_H_
+#include "Entidade.h"
+
+namespace TrabalhoJogo {
+	namespace Entidades {
+
+		class Projetil : Entidade
+		{
+		protected:
+			float velo;
+			int dano;
+			bool ativo;
+			sf::Vector2f direcao;
+
+		public:
+
+			Projetil(sf::Vector2f posicao, sf::Vector2f direcao);
+			virtual ~Projetil();
+			virtual void executar();
+			virtual void salvar();
+
+			void mover();
+			void desativar();
+			void salvarDataBuffer();
+			int getDano() const;
+		};
+	}
+}
+
+
+#endif // !PROJETIL_H_
